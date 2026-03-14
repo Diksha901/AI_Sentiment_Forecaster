df37dbfccc5942cc17789026f62e9383b8f026dd
Diksha901 <dikshaf72@gmail.com>
2026-03-14 21:42:19 +0530

    Add files via upload

--- backend/utils/cleaner.py
+++ backend/utils/cleaner.py
@@ -1,1 +1,12 @@
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import re
+
+stop_words = set(ENGLISH_STOP_WORDS)
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"[^a-zA-Z ]", "", text)
+    words = text.split()
+    words = [w for w in words if w not in stop_words and len(w) > 2]
+    return " ".join(words)
 
